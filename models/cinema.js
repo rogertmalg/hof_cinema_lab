8fcc40b3a291387b061082272bc2b7bc7c8bcf50
rogertmalg <roger.malgueira@outlook.com>
2021-11-24 14:25:11 +0000

    test 1 ok

--- models/cinema.js
+++ models/cinema.js
@@ -2,4 +2,11 @@ const Cinema = function (films) {
   this.films = films;
 };
 
+
+
+Cinema.prototype.getTitles = function (films) {
+  const result = films.map(film => film.title);
+  return result;
+}
+
 module.exports = Cinema;
